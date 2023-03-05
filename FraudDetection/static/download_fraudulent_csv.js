203c5ef39cfe024ee4e72e1dc9dc733b8bbd123d
SAGNIK GHOSAL <sagnikghosal1999@gmail.com>
2023-03-05 02:48:13 -0800

    Delete FraudDetection directory

--- FraudDetection/static/download_fraudulent_csv.js
+++
@@ -1,7 +0,0 @@
-function downloadCsv() {
-    var form = document.createElement('form');
-    form.action = '/download-csv';
-    form.method = 'POST';
-    document.body.appendChild(form);
-    form.submit();
-}
