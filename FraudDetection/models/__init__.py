e42054ce9ebdc31a3d72c121b219bd6696ed03ee
Sagnik Ghosal <sagnikghosal1999@gmail.com>
2023-03-05 01:02:21 -0800

    Testing CI for first time -- Made models and main pylint compliant

--- FraudDetection/models/__init__.py
+++ FraudDetection/models/__init__.py
@@ -5,9 +5,9 @@ lof_anomaly_detection: Uses the LOF model.
 knn_anomaly_detection: Uses the KNN model.
 copod_anomaly_detection: Uses the COPOD model.
 abod_anomaly_detection: Uses the ABOD model.
-ecod_anomaly_detection: Uses the ECOD model
+ecod_anomaly_detection: Uses the ECOD model.
 """
-
+# pylint: disable=E0401
 from .models import lof_anomaly_detection
 from .models import knn_anomaly_detection
 from .models import copod_anomaly_detection
